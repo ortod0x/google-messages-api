21fca08d65db8dce552d54f911d8002c288311d3
Pharaoh <131539265+ortod0x@users.noreply.github.com>
2023-08-16 02:24:26 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# google-messages-api
-messages.google.com API
+# Google Message Rest API
+
+### How to use
+
+- Get session
+
+```js
+const { default: MessagesClient } = require('google-messages-client')
+const fs = require('fs')
+
+const client = new MessagesClient()
+
+client.on('qr-code', (base64Image) => {
+    // example code to save image
+    fs.writeFileSync('qr.jpg', base64Image.replace(/^data:image\/png;base64,/, ""), { encoding: 'base64' })
+    // your code
+})
+
+client.on('authenticated', async (service) => {
+    const inbox = service.getInbox()
+    const credentials = await client.getCredentials()
+    fs.writeFileSync('credentials.json', JSON.stringify(credentials, null, '\t'))
+    await client.quit()
+})
+```
+You must have `credentials.json` or session file for accessing the feature. 
+
+- Read messages
+
+```js
+const { default: MessagesClient } = require('google-messages-client')
+
+const credentials = MessagesClient.loadCredentialFile('credentials.json')
+const client = new MessagesClient({ credentials })
+
+client.on('authenticated', async (service) => {
+    const inbox = await service.getInbox()
+    console.log('Inbox', inbox)
+    await client.quit()
+})
+```
+
+- Send messages
+
+```js
+const { default: MessagesClient } = require('google-messages-client')
+
+const credentials = MessagesClient.loadCredentialFile('credentials.json')
+const client = new MessagesClient({ credentials })
+
+client.on('authenticated', async (service) => {
+    console.log('Sending message...')
+    await service.sendMessage('+1337', 'Test message from SMS Client.') 
+    console.log('Done!')
+    await client.quit()
+})
+```
+
+**Note**: `sendMessage` takes first arg as number with `countryCode + Number` second arg as TextMessage
+
+- Examples are given [here](https://github.com/ortod0x/google-messages-client/tree/main/examples).
---
+++ server.js
@@ -0,0 +1,82 @@
+const { default: MessagesClient } = require('google-messages-client')
+const fs = require('fs')
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = 3000;
+const client = new MessagesClient()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Send message (Still Error)
+// app.post('/send-message', async (req, res) => {
+//     try {
+//         const credentials = MessagesClient.loadCredentialFile('credentials.json')
+//         const clients = new MessagesClient({ credentials })
+
+//         const number = req.body.number;
+//         const message = req.body.message;
+
+//         clients.on('authenticated', async (service) => {
+//             const send = await service.sendMessage("+"+number, message);
+//             // await clients.quit().then(res.status(200).json({success: true, message: 'sent!'}));
+//             console.log(req.body)
+//             await res.status(200).json({success: true, message: send});
+//         });
+//     } catch (error){
+//         console.log(error);
+//     }
+// });
+
+// Read message
+app.get('/get-message', async (req, res) => {
+    try {
+        const credentials = MessagesClient.loadCredentialFile('credentials.json')
+        const clients = new MessagesClient({ credentials })
+
+        clients.on('authenticated', async (service) => {
+            const inbox = await service.getInbox();
+            await res.status(200).json({success: true, message: inbox});
+        });
+    } catch (error){
+        console.log(error);
+    }
+});
+  
+io.on('connection', function(socket) {
+    socket.emit('message', 'Connecting...');
+    const isAuthenticated = fs.existsSync('connected.dat');
+
+    if (isAuthenticated) {
+        socket.emit('message', 'Client is Authenticated!');
+        socket.emit('authenticated', 'Authenticated!')
+        console.log('Authenticated')
+    } else {
+        client.on('qr-code', (base64Image) => {
+            console.log('QR Code received, scan please!');
+            socket.emit('qrCode', base64Image);
+            socket.emit('message', 'QR Code received, scan please!');
+        })
+        client.on('authenticated', async () => {
+            const credentials = await client.getCredentials()
+            fs.writeFileSync('credentials.json', JSON.stringify(credentials, null, '\t'))
+            fs.writeFileSync('connected.dat', '1')
+            socket.emit('message', 'Client is Authenticated!');
+            socket.emit('authenticated', 'Authenticated!')
+            console.log('Authenticated')
+        })
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
